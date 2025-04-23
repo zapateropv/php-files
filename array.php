668bbbfe6f8a4a2096a5ab4ca1d65eb0a2a04bb4
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-04-23 15:39:56 +0800

    Create array.php

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+$foods = array("apple","orange","banana","coconut");
+
+//array_push($foods, "pineapple");
+//array_pop($foods);
+//array_shift($foods);
+//$foods = array_reverse($foods);
+
+foreach($foods as $food)
+{
+    echo "{$food} <br>";
+    echo count($foods);
+}
+
+
+?>
