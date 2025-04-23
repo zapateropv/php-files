77ab3608cbec900f20ac01081e58f5a25b83ec80
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-04-23 13:31:05 +0800

    Create data types.php

---
+++ data types.php
@@ -0,0 +1,26 @@
+<?php
+
+
+//PHP VARIABLES
+
+$name = "Bro";
+$food = "pizza";
+
+$age = 21;
+$users = 10;
+
+$grade = 89.4;
+$price = 10;
+
+$isOnline = true;
+
+echo "hello {$name} <br>";
+echo "You like {$food} <br>";
+echo "Your age is {$age} <br>";
+echo "There are {$users} users online<br>";
+echo "Your grade is {$grade}<br>";
+echo "Your pizza is \${$price}<br>";
+echo "Is online?: {$isOnline}";
+
+
+?>
