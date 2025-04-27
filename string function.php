486402ebcc3192f267a5f8bff4feee0f6cf64c74
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-04-27 13:41:53 +0800

    Create string function.php

---
+++ string function.php
@@ -0,0 +1,23 @@
+<?php
+//string function
+
+$username = "Bro Code";
+$phone = "123-456-7890";
+//$username = strtolower($username);
+//$username = strtoupper($username);
+//$username = trim($username);
+//$username = str_pad($username, 20, 0);
+//$phone  = str_replace("-", "", $phone );
+//$username = strrev($username);
+//$username = str_shuffle($username);
+//$username = strcmp($username, "Bro Code");
+//$username = strlen($username);
+//$username = strpos($username, " ");
+//$username = substr($username, 0, 3);
+
+
+echo $username;
+
+
+
+?>
